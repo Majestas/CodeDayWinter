bd1cfad35b07abf65d16b0824503d6800266285f
mpearce25 <pearcematthew25@gmail.com>
2016-02-13 18:33:19 -0500

    gears working

--- src/RobotRescue.java
+++ src/RobotRescue.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
 
 public class RobotRescue {
 
-	public static void main (String[] args){
+	public static void main (String[] args) throws IOException{
 
+		Display.initDisplay();
 	}
 }
+
+
