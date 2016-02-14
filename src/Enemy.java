0f77024ee1643f2d30a7b9e4a00a5373ee17a254
Majestas <emwaite12@gmail.com>
2016-02-13 17:52:01 -0800

    Enemy Class Made

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,37 @@
 
 public class Enemy {
-
+	private int health;
+	private int attack;
+	private int speed;
+	private int defense;
+	public Enemy(int h, int a, int d, int s) {
+		this.setHealth(h);
+		this.setAttack(a);
+		this.setDefense(d);
+		this.setSpeed(s);
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
 }
